caada53c74b6b5a2ad2eedebe51247df2e56da75
juan camilo lopez gonzalez <juan.lopez1@estudiantesunibague.edu.co>
2025-09-13 18:38:55 -0500

    Se agrego una lista de 3 empleados

---
+++ Proyecto.py
@@ -0,0 +1,9 @@
+#Definir lista de empleados
+
+lista_empleados = [{"nombre": "Juan Camilo Lopez Gonzalez", "edad": 20, "sexo": "hombre", "numero telefonico": "123456789", "cargo": "desarrollador"},
+                   {"nombre": "Estefania Rodriguez", "edad": 18, "sexo": "mujer", "numero telefonico": "987654321", "cargo": "analista"},
+                   {"nombre": "Sarah Ulloque", "edad": 21, "sexo": "mujer", "numero telefonico": "543219876", "cargo": "gerente"}
+                   ] #Se agrupan diccionarios dentro de la lista con la informacion de cada empleado usando claves para acceder a los valores
+
+
+
